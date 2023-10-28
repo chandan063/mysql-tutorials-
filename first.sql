8a1bfe0ab876b516b389bd6a140448e0c5d0717f
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-10-28 13:27:24 +0530

    Update first.sql
    added some comments;

--- first.sql
+++ first.sql
@@ -1,5 +1,7 @@
 create database college;
+/*  The above line is for creating the database */
 use college;
+/* use is used to use the above database only*/
 create table student(
  id int primary key,
  name varchar(50),
@@ -10,4 +12,4 @@ create table student(
  INSERT INTO student VALUES(2,"HAN",23);
  INSERT INTO student VALUES(3,"CDAN",26);
  
- SELECT * FROM student;
+ SELECT * FROM student;/*  select is part of DQL Data query language and is used to query all the data or select all data of a table*/
