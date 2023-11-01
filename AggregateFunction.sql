3bf12ddd31af1f71727e38547d0774cc6c50a89a
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-11-01 09:52:06 +0530

    Create AggregateFunction.sql
    aggregate and group by tutorial

---
+++ AggregateFunction.sql
@@ -0,0 +1,31 @@
+/* 
+AGGREGATE FUNCTIONS---Aggregate function performs a calculation on set of values, and return a single value.
+
+>COUNT()
+>MAX()
+>MIN()
+>SUM()
+>AVG()
+
+
+*/
+SELECT max(marks) FROM student;
+-- give you the maximum values among the marks.
+SELECT avg(marks) FROM student;
+-- return the average of the marks
+SELECT min(marks) FROM student;
+-- return the min among the marks.
+SELECT count(marks) FROM student;
+
+/*
+=============================GROUP BY CLAUSE================================
+Groups rows that have the same values into summary rows.
+It collect data from multiple records and groups the result by one or more column.
+
+Generally we use group by with some aggregate functions.
+*/
+-- Example - Count number of student in each city.
+
+SELECT city, count(name) FROM student GROUP BY city;
+-- on the basis two also we can make the group
+SELECT city,name, count(rollno) FROM student GROUP BY city,name;
